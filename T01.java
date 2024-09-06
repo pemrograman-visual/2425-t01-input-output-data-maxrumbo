9b2884b77b37e573db49b3b6e784b53aae509b3c
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-09-06 11:43:55 -0700

    Sudah menyelesaikan T01

--- T01.java
+++ T01.java
@@ -1,1 +1,34 @@
-// NIM - Your Name
+// 12S24030- Swasti Sihombing
+// 12S24037- Maxwell Rumahorbo
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String judul;
+        String penulis;
+        int tahunterbit;
+        String penerbit;
+        String formatbukuelektronik;
+        double hargapembelian;
+        double minimummargin;
+        int stok;
+        double rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunterbit = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        formatbukuelektronik = input.nextLine();
+        hargapembelian = Double.parseDouble(input.nextLine());
+        minimummargin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + rating);
+    }
+}
